6291f687ebd45f203ab578158cfdeae98896ee0e
logan harris <loganrodharris@gmail.com>
2021-09-08 16:11:02 +1000

    created test data

---
+++ manage_lenders/tests/test_lenders.py
@@ -0,0 +1,28 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+class LenderTests(APITestCase):
+    fixtures = ['lender.json']
+
+    def test_list_lenders(self):
+        pass
+
+    def test_create_lender(self):
+        pass
+
+    def test_get_lender(self):
+        pass
+
+    def test_update_lender(self):
+        pass
+
+    def test_delete_lender(self):
+        pass
+
+    def test_bulk_csv_upload(self):
+        pass
+
+    def test_bulk_csv_download(self):
+        pass
+    
